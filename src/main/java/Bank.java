ef4a428d6d6e1df1fc3de3211df7eeeec95be4f4
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-01-29 12:38:53 +0000

    Protected and private

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -6,7 +6,7 @@ public class Bank {
     private List<BankAccount> accounts = new ArrayList<BankAccount>();
     private CSVLoader csvLoader = new CSVLoader();
 
-    private BankAccount createAccount(Person person, double interestRate) {
+    protected BankAccount createAccount(Person person, double interestRate) {
         BankAccount account;
         if (interestRate > 0){
             account = new SavingAccount(person,interestRate);
@@ -16,7 +16,7 @@ public class Bank {
         return account;
     }
 
-    private double getInterestRate(String[] rawAccount){
+    protected double getInterestRate(String[] rawAccount){
         double interestRate = 0.0;
         if (rawAccount.length == 4) {
             interestRate = Double.parseDouble(rawAccount[3]);
@@ -24,8 +24,7 @@ public class Bank {
         return interestRate;
     }
 
-    private void loadAccounts(){
-        List<String[]> rawAccounts = csvLoader.getData("accounts.csv");
+    protected void loadAccounts(List<String[]> rawAccounts){
         String[] header = rawAccounts.remove(0);
         Person person;
         for (String[] rawAccount: rawAccounts) {
@@ -35,26 +34,46 @@ public class Bank {
         }
     }
 
-    private void displayAccounts(){
-        for (BankAccount account : accounts){
-            System.out.println(account.toString());
-        }
+    protected void loadAccounts(){
+        List<String[]> rawAccounts = csvLoader.getData("accounts.csv");
+        loadAccounts(rawAccounts);
     }
 
-    private void applyInterest(){
+    protected String displayAccounts(List<BankAccount> accounts){
+        String accountDetails = "";
+        for (BankAccount account : accounts){
+            if (accountDetails == "") {
+                accountDetails = account.toString();
+            } else {
+                accountDetails += System.lineSeparator() + account.toString();
+            }
+        }
+        return accountDetails;
+    }
+
+    private void displayAccounts(){
+        String accountsDetails = displayAccounts(accounts);
+        System.out.println(accountsDetails);
+    }
+
+    protected void applyInterest(){
         for (BankAccount account : accounts){
             account.applyInterest();
         }
     }
 
+    protected void runAccounts(){
+        loadAccounts();
+        accounts.get(0).deposit(60.0);
+        accounts.get(0).transFerFunds(45,accounts.get(1));
+        displayAccounts();
+        applyInterest();
+        displayAccounts();
+    }
+
     public static void main(String[] args){
         Bank bank = new Bank();
-        bank.loadAccounts();
-        bank.accounts.get(0).deposit(60.0);
-        bank.accounts.get(0).transFerFunds(45,bank.accounts.get(1));
-        bank.displayAccounts();
-        bank.applyInterest();
-        bank.displayAccounts();
+        bank.runAccounts();
     }
 
 }
